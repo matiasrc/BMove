2624bdfd10f91217115a088ded6fe55d1a864001
matiasrc <matiasromeroc@gmail.com>
2022-08-03 14:46:45 -0300

    versiónn junio 2022

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -4,20 +4,48 @@ using namespace ofxCv;
 using namespace cv;
 
 void ofApp::setup() {
+    ofSetFrameRate(60);
+    //ofSetLogLevel(OF_LOG_VERBOSE);
 	ofSetVerticalSync(true);
-	cam.setDeviceID(1);
-    cam.setup(640, 480);
     
-	
-	// imitate() will set up previous and diff
-	// so they have the same size and type as cam
-	imitate(previous, cam);
-	imitate(diff, cam);
+    //----------------- XML -------------------
+    loadSettings();
+    
+    //----------------- CAM -------------------
+    setupCam(deviceID);
+    //viewCam            = false;  // en el XML
+    camWidth            = 640;
+    camHeight           = 480;
+
+    //deviceID            = 1;
+    deviceName          = "NO DEVICE AVAILABLE";
+
+    //hMirror             = false; // en el XML
+    //vMirror             = false; // en el XML
+       
+    isOneDeviceAvailable =  false;
+       
+    promedioDeMovimiento = 0.0f;
+    //----------------- GUI -------------------
+    //required call
+    gui.setup();
+    
+    ImGui::GetIO().MouseDrawCursor = false;
+    
+    show_cam_config_panel = false;
+    show_osc_config_panel = false;
+    show_about_window = false;
+    //show_plotline = false; //en el XML
+    
+    //----------------- OSC -------------------
+    
+    sender.setup(host, puerto);
 }
 
 void ofApp::update() {
 	cam.update();
 	if(cam.isFrameNew()) {
+
 		// take the absolute difference of prev and cam and save it inside diff
 		absdiff(cam, previous, diff);
 		diff.update();
@@ -30,24 +58,168 @@ void ofApp::update() {
 		
 		// you can only do math between Scalars,
 		// but it's easy to make a Scalar from an int (shown here)
-		diffMean *= Scalar(50);
+		diffMean *= Scalar(3);
+        
+        // use the [] operator to get elements from a Scalar
+        float diffRed = diffMean[0];
+        float diffGreen = diffMean[1];
+        float diffBlue = diffMean[2];
+        
+        promedioDeMovimiento = ofClamp(ofMap((( diffRed + diffGreen + diffBlue ) / 3) , 0, 255, 0, 1), 0.0f, 1.0f);
+        
+        if(enviarMOVIMIENTO) enviarOsc(etiquetaMOVIMIENTO, promedioDeMovimiento);
 	}
 }
 
 void ofApp::draw() {
-	ofSetColor(255);
-	cam.draw(0, 0);
-	diff.draw(640, 0);
+    
+    float x = 0;
+    float y = 0;
+    float ancho = cam.getWidth();
+    float alto = cam.getHeight();
+    
+    if(hMirror){
+        x = cam.getWidth();
+        ancho = -cam.getWidth();
+    }
+    if(vMirror){
+        y = cam.getHeight();
+        alto = - cam.getHeight();
+    }
+    
+    if(viewCam){
+        cam.draw(x, y, ancho, alto);
+    }else{
+        diff.draw(x, y, ancho, alto);
+    }
 	
-	// use the [] operator to get elements from a Scalar
-	float diffRed = diffMean[0];
-	float diffGreen = diffMean[1];
-	float diffBlue = diffMean[2];
-	
-	ofSetColor(255, 0, 0);
-	ofDrawRectangle(0, 0, diffRed, 10);
-	ofSetColor(0, 255, 0);
-	ofDrawRectangle(0, 15, diffGreen, 10);
-	ofSetColor(0, 0, 255);
-	ofDrawRectangle(0, 30, diffBlue, 10);
+    ofSetColor(255);
+    ofDrawRectangle(0, 10, ofMap(promedioDeMovimiento, 0, 1, 0, ofGetWidth()), 10);
+    drawGui();
+}
+
+void ofApp::setupCam(int devID){
+    
+    wdevices = cam.listDevices();
+    for(int i=0;i<static_cast<int>(wdevices.size());i++){
+        if(wdevices[i].bAvailable){
+            isOneDeviceAvailable = true;
+            devicesVector.push_back(wdevices[i].deviceName);
+            devicesID.push_back(i);
+
+            for(size_t f=0;f<wdevices[i].formats.size();f++){
+                ofLog(OF_LOG_NOTICE,"Capture Device format vailable: %ix%i",wdevices[i].formats.at(f).width,wdevices[i].formats.at(f).height);
+            }
+        }
+    }
+    
+    cam.setDeviceID(devID);
+    cam.setup(camWidth, camHeight);
+}
+void ofApp::resetCameraSettings(int devID){
+    if(devID!=deviceID){
+        ofLog(OF_LOG_NOTICE,"Changing Device to: %s",devicesVector[devID].c_str());
+
+        deviceID = devID;
+        deviceName = devicesVector[deviceID];
+    }
+    
+    if(cam.isInitialized()){
+        cam.close();
+
+        //cam = new ofVideoGrabber();
+        cam.setDeviceID(deviceID);
+        cam.setup(camWidth, camHeight);
+    }
+}
+
+//--------------------------------------------------------------
+void ofApp::enviarOsc(string etiqueta, float valor){
+    ofxOscMessage m;
+    m.setAddress(etiqueta);
+    m.addFloatArg(valor);
+    sender.sendMessage(m, false);
+}
+//--------------------------------------------------------------
+void ofApp::enviarOsc(string etiqueta, vector<float> valores){
+    ofxOscMessage m;
+    m.setAddress(etiqueta);
+    for( int i=0; i<valores.size(); i++){
+        m.addFloatArg(valores[i]);
+    }
+    sender.sendMessage(m, false);
+    
+}
+//--------------------------------------------------------------
+void ofApp::loadSettings(){
+    //-----------
+    //the string is printed at the top of the app
+    //to give the user some feedback
+    xmlMessage = "loading mySettings.xml";
+
+    //we load our settings file
+    //if it doesn't exist we can still make one
+    //by hitting the 's' key
+    if( XML.loadFile("mySettings.xml") ){
+        xmlMessage = "mySettings.xml loaded!";
+    }else{
+        xmlMessage = "unable to load mySettings.xml check data/ folder";
+    }
+
+    //read the colors from XML
+    //if the settings file doesn't exist we assigns default values (170, 190, 240)
+    //red        = XML.getValue("BACKGROUND:COLOR:RED", 170);
+    
+    
+    puerto = XML.getValue("OSC:PUERTO", 3333);
+    host = XML.getValue("OSC:HOST", "127.0.0.1");
+    
+    etiquetaMOVIMIENTO = XML.getValue("OSC:ETIQUETA:MOVIMIENTO", "/bmove/movimiento");
+    
+    enviarMOVIMIENTO = XML.getValue("OSC:ENVIARMOVIMIENTO", true);
+    
+    
+    deviceID = XML.getValue("CAM:DEVICEID", 0);
+    hMirror = XML.getValue("CAM:HMIRROR", false);
+    vMirror = XML.getValue("CAM:VMIRROR", false);
+    
+    show_plotline = XML.getValue("GUI:VISUALIZADOR", false);
+    
+    viewCam = XML.getValue("CAM:CAMARA", false);
+    
+    ofLog(OF_LOG_NOTICE,xmlMessage);
+}
+//--------------------------------------------------------------
+void ofApp::saveSettings(){
+    //XML.setValue("BACKGROUND:COLOR:RED", red);
+    XML.clear();
+
+    XML.setValue("OSC:PUERTO", puerto);
+    XML.setValue("OSC:HOST", host);
+    
+    XML.setValue("OSC:ETIQUETA:MOVIMIENTO", etiquetaMOVIMIENTO);
+
+    XML.setValue("OSC:ENVIARMOVIMIENTO", enviarMOVIMIENTO);
+    
+    
+    XML.setValue("CAM:DEVICEID", deviceID);
+    XML.setValue("CAM:HMIRROR", hMirror);
+    XML.setValue("CAM:VMIRROR", vMirror);
+    
+    XML.setValue("GUI:VISUALIZADOR", show_plotline);
+    
+    XML.setValue("CAM:CAMARA", viewCam);
+    
+    XML.saveFile("mySettings.xml");
+    xmlMessage ="settings saved to xml!";
+    ofLog(OF_LOG_NOTICE,xmlMessage);
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(ofKeyEventArgs& e){
+    //no data gets saved unless you hit the s key
+    if(e.key == 's' && e.hasModifier(OF_KEY_COMMAND)){
+        saveSettings();
+    }
 }
--- src/ofApp.h
+++ src/ofApp.h
@@ -2,17 +2,65 @@
 
 #include "ofMain.h"
 #include "ofxCv.h"
+#include "ofxImGui.h"
+#include "ofxOsc.h"
+#include "ofxXmlSettings.h"
 
 class ofApp : public ofBaseApp {
 public:
-	void setup();
-	void update();
-	void draw();
+    void setup();
+    void update();
+    void draw();
+    void keyPressed(ofKeyEventArgs& e);
+    void setupCam(int devID);
+    void resetCameraSettings(int devID);
 	
-	ofVideoGrabber cam;
-	ofPixels previous;
-	ofImage diff;
+    //----------------- CV -------------------
+    ofVideoGrabber          cam;
+    vector<ofVideoDevice>   wdevices;
+    vector<string>          devicesVector;
+    vector<int>             devicesID;
+    int                     camWidth, camHeight;
+    string                  deviceName;
+    int                     deviceID;
+    bool                    needReset;
+    bool                    isOneDeviceAvailable;
+    bool                    hMirror, vMirror;
+    
+    ofPixels previous;
+    ofImage diff;
 	
-	// a scalar is like an ofVec4f but normally used for storing color information
-	cv::Scalar diffMean;
+    // a scalar is like an ofVec4f but normally used for storing color information
+    cv::Scalar diffMean;
+    
+    float promedioDeMovimiento;
+
+    //----------------- GUI -------------------
+    void drawGui();
+    ofxImGui::Gui gui;
+
+    bool show_cam_config_panel;
+    bool show_osc_config_panel;
+    bool show_about_window;
+    bool show_plotline;
+        
+    //----------------- OSC -------------------
+    ofxOscSender sender;
+    int puerto;
+    string host;
+    string etiquetaMOVIMIENTO;
+    bool enviarMOVIMIENTO;
+    
+    void enviarOsc(string etiqueda, float valor);
+    void enviarOsc(string etiqueda, vector<float> valores);
+    
+    
+    
+    //----------------- XML -------------------
+    ofxXmlSettings XML;
+    void saveSettings();
+    void loadSettings();
+    string xmlMessage;
+    bool viewCam;
+    
 };
